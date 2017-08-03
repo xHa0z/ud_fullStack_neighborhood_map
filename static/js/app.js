5eb59d423e45fd76ad91cabfd98156d9d23e8197
xuez <xuezhihao@gmail.com>
2017-08-03 00:13:37 -0500

    refactor

--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,40 @@
 var restaurants = [
     {
         name: "The Purple Pig",
-        coord: [41.89148,-87.624826]
+        coord: {lat: 41.89148,lng: -87.624826}
     },
     {
         name: "Gyu-Kaku Japanese BBQ",
-        coord: [41.892927, -87.622315]
+        coord: {lat: 41.892927, lng: -87.622315}
     },
     {
         name: "Giordano's",
-        coord:[41.896137,-87.625673]
+        coord:{lat: 41.896137,lng: -87.625673}
     },
     {
         name: "Downtown Dogs",
-        coord: [41.89715,-87.625855]
+        coord: {lat: 41.89715,lng: -87.625855}
     },
     {
         name: "Tru",
-        coord: [41.89461, -87.622920]
+        coord: {lat: 41.89461, lng: -87.622920}
     },
     {
         name: "The Signature Room at the 95th®",
-        coord: [41.899113,-87.62287]
+        coord: {lat: 41.899113,lng: -87.62287}
     },
     {
         name: "Sprinkles Cupcakes",
-        coord: [41.900303,-87.626299]
+        coord: {lat: 41.900303,lng: -87.626299}
     }
 ];
+var markers = [];
 
 var clientID = "DNDKALMOP1ZIYDHQAYHQK5YX0L4ARY3NHYFIGPUSDV45OVEH";
 var clientSecret = "52RFOTUN5UNBXHOWWPCABS2D1HMT03MPKNU4CZEGBZPKMPI1";
 
 var map;
+
 
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
@@ -41,6 +43,34 @@ function initMap() {
         zoom: 16,
         mapTypeControl: false
     });
+
+    var infoWindow = new google.maps.InfoWindow();
+    var defaultIcon = makeMarkerIcon('0091ff');
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+    for (var i = 0; i < restaurants.length; i++) {
+
+        var pos = restaurants[i].coord;
+        var title = restaurants[i].name;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: pos,
+            title: title,
+            animaation: google.maps.Animation.DROP,
+            icon: defaultIcon
+        });
+        markers.push({'marker': marker, 'name' : marker.title});
+        marker.addListener('click', function() {
+            populateInfoWindow(this, infoWindow);
+        });
+        marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+        });
+        marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+        });
+
+    }
+
     ko.applyBindings(new AppViewModel());
 }
 
@@ -48,108 +78,28 @@ function errorHandler() {
     console.log("Something wrong with Google Map. Please try again later.");
 }
 
-var Restaurant = function (restaurant) {
-    var self = this;
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    var fsURL = "https://api.foursquare.com/v2/venues/search?v=20131016&ll=" + marker.getPosition().lat() + "," +
+        marker.getPosition().lng() + "&query=" +
+        marker.title + "&intent=match" + "&client_id=" + clientID + "&client_secret=" + clientSecret;
 
-    var defaultIcon = makeMarkerIcon('0091ff');
-    var highlightedIcon = makeMarkerIcon('FFFF24');
 
-    this.name = restaurant.name;
-    this.lat = restaurant.coord[0];
-    this.lng = restaurant.coord[1];
-    this.street = "";
-    this.city = "";
-    this.phone = "";
-
-    this.visible = ko.observable(true);
-
-    var fourSquarURL = "https://api.foursquare.com/v2/venues/search?v=20131016&ll=" + this.lat + "," + this.lng + "&query=" +
-        this.name + "&intent=match" + "&client_id=" + clientID + "&client_secret=" + clientSecret;
-
-    $.getJSON(fourSquarURL).done(function (restaurant) {
-        var results = restaurant.response.venues[0];
-        self.street = results.location.formattedAddress[0];
-        self.city = results.location.formattedAddress[1];
-        self.phone = results.contact.phone;
+    $.getJSON(fsURL).done(function(data){
+        var result = data.response.venues[0];
+        infowindow.setContent('<div class="info-window-content"><div class="title" style="text-align: center"><b>' +
+            marker.title + "</b></div>" +
+            '<div class="street" style="text-align: center">' + result.location.formattedAddress[0] + "</div>" +
+            '<div class="city" style="text-align: center">' + result.location.formattedAddress[1] + "</div>" +
+            '<div class="phone" style="text-align: center">' + result.contact.phone + "</div></div>")
+    });
+    infowindow.marker = marker;
+    infowindow.open(map, marker);
+    infowindow.addListener('closeclick',function(){
+        infowindow.setMarker = null;
+        infowindow.setContent("");
     });
 
-
-    this.infoWindow = new google.maps.InfoWindow();
-
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(this.lat, this.lng),
-        map: map,
-        title: this.name,
-        animation: google.maps.Animation.DROP,
-        icon: defaultIcon
-    });
-
-    this.marker.addListener('click', function () {
-
-        self.infoWindow.setContent('<div class="info-window-content"><div class="title" style="text-align: center"><b>' +
-            restaurant.name + "</b></div>" +
-            '<div class="street" style="text-align: center">' + self.street + "</div>" +
-            '<div class="city" style="text-align: center">' + self.city + "</div>" +
-            '<div class="phone" style="text-align: center">' + self.phone + "</div></div>");
-
-        self.infoWindow.open(map, this);
-
-        self.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function () {
-            self.marker.setAnimation(null);
-        }, 1000);
-    });
-
-
-    this.marker.addListener('mouseover', function () {
-      this.setIcon(highlightedIcon);
-    });
-
-    this.marker.addListener('mouseout', function () {
-        this.setIcon(defaultIcon);
-    });
-
-    this.showMarker = ko.computed(function () {
-        if (this.visible() === true) {
-            this.marker.setMap(map);
-        } else {
-            this.marker.setMap(null);
-        }
-        return true;
-    }, this);
-
-    this.bounce = function (restaurant) {
-        google.maps.event.trigger(self.marker, 'click');
-    };
-};
-
-
-function AppViewModel() {
-    var self = this;
-
-    this.query = ko.observable("");
-
-    this.restaurantList = ko.observableArray([]);
-
-    restaurants.forEach(function (restaurant) {
-        self.restaurantList.push(new Restaurant(restaurant));
-    });
-
-    this.filteredList = ko.computed(function () {
-        var query = self.query().toLowerCase();
-        if (query === "") {
-            self.restaurantList().forEach(function (restaurant) {
-                restaurant.visible(true);
-            });
-            return self.restaurantList();
-        } else {
-            return ko.utils.arrayFilter(self.restaurantList(), function (restaurant) {
-                var result = restaurant.name.toLowerCase().search(query) >= 0;
-                restaurant.visible(result);
-                return result;
-            });
-        }
-    }, self);
 }
 
 function makeMarkerIcon(markerColor) {
@@ -163,3 +113,50 @@ function makeMarkerIcon(markerColor) {
     return markerImage;
 }
 
+function setVisible(marker, isVisible) {
+    if (isVisible) {
+        marker.setMap(map);
+    } else {
+        marker.setMap(null);
+    }
+}
+
+function AppViewModel() {
+    var self = this;
+
+    self.rstList = ko.observableArray([]);
+    self.query = ko.observable("")
+
+    restaurants.forEach(function(rst) {
+       self.rstList.push(new Restaurant(rst))
+    });
+
+    this.filteredList = ko.computed(function () {
+        var query = self.query().toLowerCase();
+        if (query === "") {
+            console.log(111);
+            for (var i = 0; i < markers.length; i++) {
+                setVisible(markers[i].marker, true);
+            }
+            return  self.rstList();
+        } else {
+            console.log(222);
+            for (i = 0; i < markers.length; i++) {
+                var name = markers[i].name;
+                console.log(name);
+                var result = name.toLowerCase().search(query) >= 0;
+                setVisible(markers[i].marker, result);
+                console.log(result)
+            }
+            return  self.rstList()
+        }
+    }, self);
+
+
+
+
+}
+
+var Restaurant = function(data) {
+    this.name = ko.observable(data.name);
+}
